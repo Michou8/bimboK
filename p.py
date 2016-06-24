1adf66f23fae233964476d0ffd883e73646d60ad
Misu Helal <mhelal@giraud-sisyphe.vm.vdi.s1.p.fti.net>
2016-06-24 17:13:52 +0200

    give pieces and weight

--- p.py
+++ p.py
@@ -8,19 +8,32 @@ with open('producto_tabla.csv') as f:
 	header = f.next().strip().split(',')
 	for line in f:
 		line = line.strip().split(',')
-		pr = line[1]
-		data[line[0]] = {}
-		for word in pr.split():
-			word = word.lower()
-			if word not in data[line[0]]:
-				data[line[0]][word] = 1
+		word = line[1]
+		data[line[0]] = 0
+		#for word in pr.split():
+		#word = word.lower()
+		match = re.search('[0-9]+(g|ml|kg)',word.lower())
+		matchp = re.search('[0-9]+p',word.lower())
+		p = -1
+		if matchp:
+			p= int(matchp.group().replace('p',''))
+			if p ==0:
+				p = 1
 				
+		weight = -1
+		if match:
+			if 'kg' in match.group():
+				weight = int(match.group().replace('ml','').replace('kg',''))*1000
 			else:
-				data[line[0]][word] += 1
-			if word not in vocab:
-				vocab[word] = 1
-			else:
-				vocab[word] += 1
+				if 'ml' in match.group() or 'g' in match.group():
+	        			weight = int(match.group().replace('ml','').replace('g',''))
+		if p<0 and weight <0:
+			data[line[0]] = -weight*p
+		else:
+			data[line[0]] = weight*p
+		
+		print data[line[0]]
+		print line
 
 with open('vocab.json','wb') as f:
 	json.dump(vocab,f,indent=2)
@@ -32,10 +45,8 @@ for key in vocab:
 	try:
 		int(key)
 	except:
-		match = re.search('[0-9]+(g|ml|kg)',key)
-		matchp = re.search('[0-9]+p',key)
-		if matchp:
-			print matchp.group()
+		match = re.search('[0-9]+(g|ml|kg)',key.lower())
+		matchp = re.search('[0-9]+p',key.lower())
 		if key not in stop:
 			vocab_w_int[key] = vocab[key]
 with open('vocab_w_int.json','wb') as f:
